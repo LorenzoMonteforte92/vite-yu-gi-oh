a77eca63770285565cad12d3c87d86403035994d
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-04-15 16:05:02 +0200

    feat: add cards template

--- src/components/AppCardDisplay.vue
+++ src/components/AppCardDisplay.vue
@@ -1,6 +1,11 @@
 <script>
-    export default {
+import YugiCards from './YugiCards.vue';
+    
+export default {
         name: 'AppCardDisplay',
+        components:{
+            YugiCards,
+        },
 
         data(){
             return{
@@ -20,8 +25,8 @@
 <template>
    <div class="container">
     <div class="row">
-        <div class="col-12">
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae aut facere distinctio, dignissimos expedita sed debitis labore laboriosam perspiciatis quae! Corrupti nihil laboriosam doloribus rem, quibusdam deleniti rerum explicabo totam.
+        <div class="col-12 d-flex flex-wrap p-4 gap-3">
+            <YugiCards></YugiCards>
         </div>
     </div>
    </div>
---
+++ src/components/YugiCards.vue
@@ -0,0 +1,96 @@
+
+<script>
+    export default {
+        name: 'YugiCards',
+
+        data(){
+            return{
+                
+            }
+        },
+        
+        methods: {
+            
+            }
+        }
+    
+	
+
+    
+</script>
+
+<template>
+    <div class="card mt-3">
+        <img src="https://images.saymedia-content.com/.image/t_share/MTc0Mzg2MzUzNTMzMjk4MDI0/yu-gi-ohs-top-6-forbidden-monster-cards.jpg" class="card-img-top" alt="...">
+    <div class="card-body ">
+        <h5 class="text-white" >CARD NAME</h5>
+        <p class="card-text">Some quick example</p>
+    </div>
+  </div>
+
+  <div class="card mt-3">
+        <img src="https://images.saymedia-content.com/.image/t_share/MTc0Mzg2MzUzNTMzMjk4MDI0/yu-gi-ohs-top-6-forbidden-monster-cards.jpg" class="card-img-top" alt="...">
+    <div class="card-body ">
+        <h5 class="text-white" >CARD NAME</h5>
+        <p class="card-text">Some quick example</p>
+    </div>
+  </div>
+
+  <div class="card mt-3">
+        <img src="https://images.saymedia-content.com/.image/t_share/MTc0Mzg2MzUzNTMzMjk4MDI0/yu-gi-ohs-top-6-forbidden-monster-cards.jpg" class="card-img-top" alt="...">
+    <div class="card-body ">
+        <h5 class="text-white" >CARD NAME</h5>
+        <p class="card-text">Some quick example</p>
+    </div>
+  </div>
+
+  <div class="card mt-3">
+        <img src="https://images.saymedia-content.com/.image/t_share/MTc0Mzg2MzUzNTMzMjk4MDI0/yu-gi-ohs-top-6-forbidden-monster-cards.jpg" class="card-img-top" alt="...">
+    <div class="card-body ">
+        <h5 class="text-white" >CARD NAME</h5>
+        <p class="card-text">Some quick example</p>
+    </div>
+  </div>
+
+  <div class="card mt-3">
+        <img src="https://images.saymedia-content.com/.image/t_share/MTc0Mzg2MzUzNTMzMjk4MDI0/yu-gi-ohs-top-6-forbidden-monster-cards.jpg" class="card-img-top" alt="...">
+    <div class="card-body ">
+        <h5 class="text-white" >CARD NAME</h5>
+        <p class="card-text">Some quick example</p>
+    </div>
+  </div>
+
+  <div class="card mt-3">
+        <img src="https://images.saymedia-content.com/.image/t_share/MTc0Mzg2MzUzNTMzMjk4MDI0/yu-gi-ohs-top-6-forbidden-monster-cards.jpg" class="card-img-top" alt="...">
+    <div class="card-body ">
+        <h5 class="text-white" >CARD NAME</h5>
+        <p class="card-text">Some quick example</p>
+    </div>
+  </div>
+
+  <div class="card mt-3">
+        <img src="https://images.saymedia-content.com/.image/t_share/MTc0Mzg2MzUzNTMzMjk4MDI0/yu-gi-ohs-top-6-forbidden-monster-cards.jpg" class="card-img-top" alt="...">
+    <div class="card-body ">
+        <h5 class="text-white" >CARD NAME</h5>
+        <p class="card-text">Some quick example</p>
+    </div>
+  </div>
+  
+</template>
+
+<style scoped lang="scss">
+
+.card{
+    width: calc(100% / 5 - 1rem)
+}
+
+.card-body{
+    background-color: #D48F38;
+    text-align: center;
+}
+
+img{
+        height: 100%;
+        object-fit: cover;
+    }
+</style>
